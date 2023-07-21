ea3b2c52dbe0e9994f7d7ef14f8128ea1afd0aa6
Francisco Vargas G. <52474105+f-vargasg@users.noreply.github.com>
2023-07-20 21:51:46 -0600

    Ajuste con DataModules

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/module-info.java to edit this template
+ */
+
+module com.fvgprinc.tools.utilities {
+    exports com.fvgprinc.tools.utilities;
+    
+    requires org.apache.commons.codec;
+    requires java.sql;
+    requires java.desktop;
+}
