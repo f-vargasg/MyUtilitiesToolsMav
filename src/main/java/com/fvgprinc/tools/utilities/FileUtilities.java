33d53b0cde4df8f69e304dd9324b4157a4efd53a
Francisco Vargas G. <52474105+f-vargasg@users.noreply.github.com>
2024-01-03 18:07:07 -0600

    Se añade la funcion combine

--- src/main/java/com/fvgprinc/tools/utilities/FileUtilities.java
+++ src/main/java/com/fvgprinc/tools/utilities/FileUtilities.java
@@ -11,149 +11,155 @@ import java.io.FileWriter;
 import java.io.InputStream;
 
 public class FileUtilities {
-	private String errorMsg;
-	private String actualFileName;
 
-	public FileUtilities() {
-		errorMsg = new String();
-	}
+    private String errorMsg;
+    private String actualFileName;
 
-	/**
-	 * Lee el template file de disco.
-	 */
-	public String readFileText(String filename) {
-		int len;
-		char chars[] = null;
-		BufferedReader inStream;
+    public FileUtilities() {
+        errorMsg = new String();
+    }
 
-		// abrir y leer archivo
-		try {
-			File inFile = new File(filename);
-			inStream = new BufferedReader(new FileReader(inFile));
-			chars = new char[(int) inFile.length()];
-			len = inStream.read(chars);
-			inStream.close();
-		} catch (FileNotFoundException e) {
-			System.out.println(e);
-			errorMsg = "Missing File: " + filename;
-		} catch (Exception e) {
-			System.out.println(e);
-			errorMsg = "Exception File: " + filename;
-		}
-		String template = new String(chars);
+    /**
+     * Lee el template file de disco.
+     */
+    public String readFileText(String filename) {
+        int len;
+        char chars[] = null;
+        BufferedReader inStream;
 
-		return template;
-	}
+        // abrir y leer archivo
+        try {
+            File inFile = new File(filename);
+            inStream = new BufferedReader(new FileReader(inFile));
+            chars = new char[(int) inFile.length()];
+            len = inStream.read(chars);
+            inStream.close();
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+            errorMsg = "Missing File: " + filename;
+        } catch (Exception e) {
+            System.out.println(e);
+            errorMsg = "Exception File: " + filename;
+        }
+        String template = new String(chars);
 
-	/**
-	 * getDocumentByteBuffer : Obtiene la información de un archivo determinado.
-	 *
-	 * @return archivo binario guardo en un arreglo de bytes.
-	 * @param infoDocumento
-	 *            Información Básica del documento.
-	 */
-	public byte[] getDocumentByteBuffer(String infoFilePath) {
-		ByteArrayOutputStream bufferStream = new ByteArrayOutputStream();
-		try {
+        return template;
+    }
 
-			File archivo = new File(infoFilePath);
-			if (archivo.exists()) {
-				// Invocar al servicio web que trae el documento del cliente
-				// actual.
-				actualFileName = archivo.getName();
-				InputStream in = new FileInputStream(archivo);
-				byte[] buffer = new byte[1024 * 8];// 1024 bytes
-				int offset = 0;
-				int numRead = 0;
-				while ((numRead = in.read(buffer)) >= 0) {
-					bufferStream.write(buffer, 0, numRead);
-					offset += numRead;
-				}
-			}
-		} catch (FileNotFoundException ex) {
-			ex.printStackTrace(System.out);
-		} catch (Exception ex) {
-			ex.printStackTrace(System.out);
-		}
+    /**
+     * getDocumentByteBuffer : Obtiene la información de un archivo determinado.
+     *
+     * @return archivo binario guardo en un arreglo de bytes.
+     * @param infoDocumento Información Básica del documento.
+     */
+    public byte[] getDocumentByteBuffer(String infoFilePath) {
+        ByteArrayOutputStream bufferStream = new ByteArrayOutputStream();
+        try {
 
-		return bufferStream.toByteArray();
-	}
+            File archivo = new File(infoFilePath);
+            if (archivo.exists()) {
+                // Invocar al servicio web que trae el documento del cliente
+                // actual.
+                actualFileName = archivo.getName();
+                InputStream in = new FileInputStream(archivo);
+                byte[] buffer = new byte[1024 * 8];// 1024 bytes
+                int offset = 0;
+                int numRead = 0;
+                while ((numRead = in.read(buffer)) >= 0) {
+                    bufferStream.write(buffer, 0, numRead);
+                    offset += numRead;
+                }
+            }
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace(System.out);
+        } catch (Exception ex) {
+            ex.printStackTrace(System.out);
+        }
 
-	/**
-	 * Determina si existe un archivo.
-	 */
-	public boolean existFile(String filename) {
-		boolean retorno = false;
-		try {
-			File inFile = new File(filename);
-			retorno = inFile.exists();
-		} catch (Exception e) {
-			System.out.println(e);
-		}
-		return retorno;
-	}
+        return bufferStream.toByteArray();
+    }
 
-	public boolean writeFile(String buffer, String targetFile) {
+    /**
+     * Determina si existe un archivo.
+     */
+    public boolean existFile(String filename) {
+        boolean retorno = false;
+        try {
+            File inFile = new File(filename);
+            retorno = inFile.exists();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return retorno;
+    }
 
-		boolean success = true;
-		// Escritura de la salida del parser
+    public boolean writeFile(String buffer, String targetFile) {
 
-		if (buffer != null && buffer.length() > 1) {
+        boolean success = true;
+        // Escritura de la salida del parser
 
-			try {
+        if (buffer != null && buffer.length() > 1) {
 
-				BufferedWriter fileBufferedWriter;
-				File inputFile = new File(targetFile);
-				fileBufferedWriter = new BufferedWriter(new FileWriter(
-						inputFile));
-				fileBufferedWriter.write(buffer);
-				fileBufferedWriter.close();
-			} catch (Exception e) {
-				success = false;
-				System.out.println("Error a la hora de escribir archivo ..."
-						+ targetFile);
-			}
+            try {
 
-		} // if (buffer.length()>1){
-		return success;
-	}
+                BufferedWriter fileBufferedWriter;
+                File inputFile = new File(targetFile);
+                fileBufferedWriter = new BufferedWriter(new FileWriter(
+                        inputFile));
+                fileBufferedWriter.write(buffer);
+                fileBufferedWriter.close();
+            } catch (Exception e) {
+                success = false;
+                System.out.println("Error a la hora de escribir archivo ..."
+                        + targetFile);
+            }
 
-	public boolean cleanFile(String actualFile) {
-		boolean result = true;
-		try {
-			File dropFile = new File(actualFile);
-			dropFile.delete();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return result;
-	}
+        } // if (buffer.length()>1){
+        return success;
+    }
 
-	public static boolean makeTreeFiles(String path) {
-		boolean retorno = false;
-		try {
-			File fileVerificator = new File(path);
-			String parent = fileVerificator.getParent();
-			File parentPath = new File(fileVerificator.getParent());
+    public boolean cleanFile(String actualFile) {
+        boolean result = true;
+        try {
+            File dropFile = new File(actualFile);
+            dropFile.delete();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
 
-			if (!parentPath.exists()) {
-				System.out
-						.println("La ruta dada no existe, creando estructura para "
-								+ fileVerificator.getParent());
-				retorno = parentPath.mkdirs();
-			}
-		} catch (Exception ex) {
-			ex.printStackTrace(System.out);
-		}
-		return retorno;
-	}
+    public static boolean makeTreeFiles(String path) {
+        boolean retorno = false;
+        try {
+            File fileVerificator = new File(path);
+            String parent = fileVerificator.getParent();
+            File parentPath = new File(fileVerificator.getParent());
 
-	public String getErrorMsg() {
-		return errorMsg;
-	}
+            if (!parentPath.exists()) {
+                System.out
+                        .println("La ruta dada no existe, creando estructura para "
+                                + fileVerificator.getParent());
+                retorno = parentPath.mkdirs();
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace(System.out);
+        }
+        return retorno;
+    }
 
-	public String getActualFileName() {
-		return actualFileName;
-	}
+    public String getErrorMsg() {
+        return errorMsg;
+    }
+
+    public String getActualFileName() {
+        return actualFileName;
+    }
+
+    public static String combine(String path1, String path2) {
+        File file1 = new File(path1);
+        File file2 = new File(file1, path2);
+        return file2.getPath();
+    }
 
 }
